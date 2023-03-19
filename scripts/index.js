ce8557bd2f415a038b53d44cd973226e005ac61d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-03-18 18:24:27 -0700

    refactor: reformated code

--- scripts/index.js
+++ scripts/index.js
@@ -1,78 +1,79 @@
 const initialCards = [
     {
         name: "Yosemite Valley",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     },
     {
         name: "Lake Louise",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     },
     {
         name: "Bald Mountains",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
     },
     {
         name: "Latemar",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
     },
     {
         name: "Vanoise National Park",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
     },
     {
         name: "Lago di Braies",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg "
-    }
-]
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+    },
+];
 
-const profileTitleText = document.querySelector('.profile__title');
-const profileDescriptionText = document.querySelector('.profile__description');
-const profileEditBtn = document.querySelector('.profile__edit-button');
-const modal = document.querySelector('.modal');
-const modalForm = modal.querySelector('.modal__form')
-const modalCloseBtn = document.querySelector('.modal__close');
-const modalTitleInput = document.querySelector('#profile-title-input');
-const modalDescriptionInput = document.querySelector('#profile-description-input');
-const cardListElement = document.querySelector('.cards__list');
-const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
+const profileTitleText = document.querySelector(".profile__title");
+const profileDescriptionText = document.querySelector(".profile__description");
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const modal = document.querySelector(".modal");
+const modalForm = modal.querySelector(".modal__form");
+const modalCloseBtn = document.querySelector(".modal__close");
+const modalTitleInput = document.querySelector("#profile-title-input");
+const modalDescriptionInput = document.querySelector(
+    "#profile-description-input"
+);
+const cardListElement = document.querySelector(".cards__list");
+const cardTemplate =
+    document.querySelector("#card-template").content.firstElementChild;
 
 function getCardElement(data) {
     const cardElement = cardTemplate.cloneNode(true);
-    const cardImageElement = cardElement.querySelector('.card__image');
-    const cardTitleElement = cardElement.querySelector('.card__title');
+    const cardImageElement = cardElement.querySelector(".card__image");
+    const cardTitleElement = cardElement.querySelector(".card__title");
 
-    cardImageElement.src = data.link
-    cardImageElement.alt = data.name
-    cardTitleElement.textContent = data.name
+    cardImageElement.src = data.link;
+    cardImageElement.alt = data.name;
+    cardTitleElement.textContent = data.name;
 
     return cardElement;
 }
 
 function handleCloseModal() {
-    modal.classList.remove('modal_opened');
+    modal.classList.remove("modal_opened");
 }
 
-profileEditBtn.addEventListener('click', function () {
-    modalTitleInput.value = String(profileTitleText.textContent)
+profileEditBtn.addEventListener("click", function () {
+    modalTitleInput.value = String(profileTitleText.textContent);
     modalDescriptionInput.value = String(profileDescriptionText.textContent);
 
-    modal.classList.add('modal_opened');
+    modal.classList.add("modal_opened");
 });
 
-modalCloseBtn.addEventListener('click', handleCloseModal);
+modalCloseBtn.addEventListener("click", handleCloseModal);
 
-modalForm.addEventListener('submit', (evt) => {
+modalForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
 
     profileTitleText.textContent = modalTitleInput.value;
     profileDescriptionText.textContent = modalDescriptionInput.value;
 
     closeModal();
-})
-
-
+});
 
 initialCards.forEach((data) => {
     const cardElement = getCardElement(data);
     cardListElement.prepend(cardElement);
-})
+});
