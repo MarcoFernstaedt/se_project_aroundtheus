284021a8431784164d126c2c8af5e95fa50ab989
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-03-18 17:27:54 -0700

    feat: adding cards

--- scripts/index.js
+++ scripts/index.js
@@ -29,20 +29,36 @@ const profileTitleText = document.querySelector('.profile__title');
 const profileDescriptionText = document.querySelector('.profile__description');
 const profileEditBtn = document.querySelector('.profile__edit-button');
 const modal = document.querySelector('.modal');
+const modalForm = modal.querySelector('.modal__form')
 const modalCloseBtn = document.querySelector('.modal__close');
 const modalTitleInput = document.querySelector('#profile-title-input');
 const modalDescriptionInput = document.querySelector('#profile-description-input');
+const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
+
+
 
 profileEditBtn.addEventListener('click', function () {
-
     modalTitleInput.value = String(profileTitleText.textContent)
     modalDescriptionInput.value = String(profileDescriptionText.textContent);
 
     modal.classList.add('modal_opened');
 });
 
-modalCloseBtn.addEventListener('click', function () {
+modalCloseBtn.addEventListener('click', closeModal);
+
+modalForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    profileTitleText.textContent = modalTitleInput.value;
+    profileDescriptionText.textContent = modalDescriptionInput.value;
+
+    closeModal();
+})
+
+function closeModal() {
     modal.classList.remove('modal_opened');
-});
+}
 
-
+for (card of initialCards) {
+    console.log(card)
+}
