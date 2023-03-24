1dc3d29dccb1b2e1c63c1f3af7df84e1aa15a1e1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-03-24 11:47:07 -0700

    bug: like button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -43,11 +43,16 @@ const cardListElement = document.querySelector(".cards__list");
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 const cardTitleInput = addCardModal.querySelector('#card-title-input');
 const cardImageInput = addCardModal.querySelector('#card-image-input');
-
+// https://images.unsplash.com/photo-1550330545-87c6109a81f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80
 const getCardElement = data => {
     const cardElement = cardTemplate.cloneNode(true);
     const cardImageElement = cardElement.querySelector(".card__image");
     const cardTitleElement = cardElement.querySelector(".card__title");
+    const likeButton = document.querySelector('.card__like-button');
+    console.log(likeButton)
+    // likeButton.addEventListener('click', () => {
+    //     likeButton.classList.toggle('card__like-button_active');
+    // });
 
     cardImageElement.src = data.link;
     cardImageElement.alt = data.name;
@@ -101,6 +106,6 @@ addCardModalForm.addEventListener('submit', (evt) => {
 
 initialCards.forEach((data) => {
     renderCard(data, cardListElement)
-    // const cardElement = getCardElement(data);
-    // cardListElement.prepend(cardElement);
+    console.log(data)
 });
+
