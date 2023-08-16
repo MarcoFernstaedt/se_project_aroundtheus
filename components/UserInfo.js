d5f8b1d370fd9dfd9a1cb5def816adee7536b19c
Marco Fernstaedt <marcofernstaedt@Austyns-MacBook-Air-6.local>
2023-08-16 14:16:04 -0700

    refactor: user info now stored in a class

---
+++ components/UserInfo.js
@@ -0,0 +1,20 @@
+import Popup from "/Popup.js";
+
+export default class UserInfo {
+    constructor({name, job}) {
+        this._name = name;
+        this._job = job;
+    }
+
+    getUserInfo() {
+        return {
+            name: this._name.textContent, 
+            job: this._job.textContent,
+        };
+    }
+
+    setUserInfo(name, job) {
+        this._name.textContent = name;
+        this._job.textContent = job;
+    }
+}
