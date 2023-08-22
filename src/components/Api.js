ee4af86b1572d42a1450006741a1cedcba380316
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-08-22 13:43:32 -0700

    create: _handleResponce method Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,4 +3,12 @@ export default class Api {
     this._baseUrl = baseUrl;
     this._header = header;
   }
+
+  _handleResponce(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return res.status;
+    }
+  }
 }
